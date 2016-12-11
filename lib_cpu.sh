fb33f34ce74ec694e51f5de8e45f8e2358e27829
Pablo Niklas <pablo.niklas@gmail.com>
2016-12-11 08:41:58 -0300

    Spaces

--- lib_cpu.sh
+++ lib_cpu.sh
@@ -1,5 +1,5 @@
 ##############################################
-# Librerias de bajo nivel 
+# Librerias de bajo nivel
 # By Pablo Niklas - Bajo licencia GPL
 ##############################################
 
@@ -26,26 +26,26 @@
 function paralelo() {
 
 # PARALELO:
-# Cantidad de procesos en paralelo. 
+# Cantidad de procesos en paralelo.
 # Depende de el SO y/o la arquitectura. Puede ser:
 # 1) Cant. de cpus + 1 (x86)
 # 2) Cant. de cpus * 2 (x86)
 # 3) Cant. de cpus     (SPARC)
 if [ -z "$PARALELO" ]; then
-	if [ "`uname -s`"="SunOS" ]; then
-		# Para SunOS...
-		if [ "`uname -r`" = "5.6" ]; then
-			PARALELO=`/usr/platform/`uname -m`/sbin/prtdiag -v|grep "US-"|wc -l`	# Solaris 2.6
-		else
-			if [ "`uname -r`" = "5.9" ]; then
-				PARALELO=`/usr/platform/`uname -m`/sbin/prtdiag -v|grep ^CPU|wc -l`	# Solaris 9
-			else
-				PARALELO=1	# Default
-			fi
-		fi
-	else
-		PARALELO=$((`cat /proc/cpuinfo |grep ^proces|wc -l`*2)) # Linux
-	fi
+    if [ "`uname -s`"="SunOS" ]; then
+        # Para SunOS...
+        if [ "`uname -r`" = "5.6" ]; then
+            PARALELO=`/usr/platform/`uname -m`/sbin/prtdiag -v|grep "US-"|wc -l`    # Solaris 2.6
+        else
+            if [ "`uname -r`" = "5.9" ]; then
+                PARALELO=`/usr/platform/`uname -m`/sbin/prtdiag -v|grep ^CPU|wc -l`    # Solaris 9
+            else
+                PARALELO=1    # Default
+            fi
+        fi
+    else
+        PARALELO=$((`cat /proc/cpuinfo |grep ^proces|wc -l`*2)) # Linux
+    fi
 fi
 
 # DIRLOG:
@@ -61,73 +61,71 @@ echo ":::: Se correran $# procesos en total ($PARALELO en forma concurrente)." >
 # Inicializo variables del sistema
 I=0;SEQ=""
 while [ $I -le $(($PARALELO-1)) ] ; do
-	PID[$I]=0
-	SEQ=$SEQ+"$I " # Como no tengo seq, lo genero :)
-	I=$(($I+1))
+    PID[$I]=0
+    SEQ=$SEQ+"$I " # Como no tengo seq, lo genero :)
+    I=$(($I+1))
 done
 SEQ=`echo $SEQ|sed 's/+//g'` # Depuro los "+"
-
-# Comentario de prueba
 
 TERMINO=false
 JOBLOGTMP="job.$$.`date +'%d'`"
 TAREA=0
 while [ $# != 0 ] || ! $TERMINO; do
-	A=0
-	for A in $SEQ; do
-		# Asigno procesos si tengo lugar.
-		if [ ${PID[$A]} -eq 0 ] && [ $# != 0 ]; then
-			TAREA=$(($TAREA+1))
-			echo "::: Job #$TAREA - Thread #$A - `date +'%d/%m/%Y - %H:%M:%S'` - INICIADO." >> $DIRLOG/$JOBLOGTMP.`printf %.3d $TAREA`.log
-			#echo ": COMIENZO detalle del Job." >> $DIRLOG/$JOBLOGTMP.`printf %.3d $TAREA`.log
-			#echo $1 >> $DIRLOG/$JOBLOGTMP.`printf %.3d $TAREA`.log
-			#echo ": FIN detalle del Job." >> $DIRLOG/$JOBLOGTMP.`printf %.3d $TAREA`.log
-			#echo ": COMIENZO salida del Job." >> $DIRLOG/$JOBLOGTMP.`printf %.3d $TAREA`.log
-			$1 1>> $DIRLOG/$JOBLOGTMP.`printf %.3d $TAREA`.log 2>&1 &
-			PID[$A]=$!
-			TID[$!]=$TAREA
+    A=0
+    for A in $SEQ; do
+        # Asigno procesos si tengo lugar.
+        if [ ${PID[$A]} -eq 0 ] && [ $# != 0 ]; then
+            TAREA=$(($TAREA+1))
+            echo "::: Job #$TAREA - Thread #$A - `date +'%d/%m/%Y - %H:%M:%S'` - INICIADO." >> $DIRLOG/$JOBLOGTMP.`printf %.3d $TAREA`.log
+            #echo ": COMIENZO detalle del Job." >> $DIRLOG/$JOBLOGTMP.`printf %.3d $TAREA`.log
+            #echo $1 >> $DIRLOG/$JOBLOGTMP.`printf %.3d $TAREA`.log
+            #echo ": FIN detalle del Job." >> $DIRLOG/$JOBLOGTMP.`printf %.3d $TAREA`.log
+            #echo ": COMIENZO salida del Job." >> $DIRLOG/$JOBLOGTMP.`printf %.3d $TAREA`.log
+            $1 1>> $DIRLOG/$JOBLOGTMP.`printf %.3d $TAREA`.log 2>&1 &
+            PID[$A]=$!
+            TID[$!]=$TAREA
 
-			echo "::: Job #$TAREA - Thread #$A - `date +'%d/%m/%Y - %H:%M:%S'` - INICIADO."  >> $LOGCPU
+            echo "::: Job #$TAREA - Thread #$A - `date +'%d/%m/%Y - %H:%M:%S'` - INICIADO."  >> $LOGCPU
 
-			shift
-		fi
-		A=$(($A+1))
-	done
+            shift
+        fi
+        A=$(($A+1))
+    done
 
-	# Ciclo de control de finalizacion de cada hilo.
-	TERMINO=true
-	A=0
-	for A in $SEQ; do
+    # Ciclo de control de finalizacion de cada hilo.
+    TERMINO=true
+    A=0
+    for A in $SEQ; do
 
-		# Los distintos *nix, manejan los procesos a su manera. :)
-		FINALIZO=false
-		if [ "`uname -s`" = "SunOS" ] && [ ${PID[$A]} -gt 0 ]; then
-			[ -z "`ps -p ${PID[$A]}|grep -v "   PID TTY      TIME CMD"`" ] && FINALIZO=true
-		fi
+        # Los distintos *nix, manejan los procesos a su manera. :)
+        FINALIZO=false
+        if [ "`uname -s`" = "SunOS" ] && [ ${PID[$A]} -gt 0 ]; then
+            [ -z "`ps -p ${PID[$A]}|grep -v "   PID TTY      TIME CMD"`" ] && FINALIZO=true
+        fi
 
-		if [ "`uname -s`" = "Linux" ] && [ ${PID[$A]} -gt 0 ]; then
-			[ -z "`ps --no-heading --pid ${PID[$A]}`" ] && FINALIZO=true
-		fi
+        if [ "`uname -s`" = "Linux" ] && [ ${PID[$A]} -gt 0 ]; then
+            [ -z "`ps --no-heading --pid ${PID[$A]}`" ] && FINALIZO=true
+        fi
 
-		if $FINALIZO ; then
-			#echo ": FIN salida del Job." >> $DIRLOG/$JOBLOGTMP.`printf %.3d ${TID[${PID[$A]}]}`.log
-			echo "::: Job #${TID[${PID[$A]}]} - Thread #$A - `date +'%d/%m/%Y - %H:%M:%S'` - FINALIZADO." >> $DIRLOG/$JOBLOGTMP.`printf %.3d ${TID[${PID[$A]}]}`.log
-			echo >> $DIRLOG/$JOBLOGTMP.`printf %.3d ${TID[${PID[$A]}]}`.log
-			echo "::: Job #${TID[${PID[$A]}]} - Thread #$A - `date +'%d/%m/%Y - %H:%M:%S'` - FINALIZADO."  >> $LOGCPU
- 			cat $DIRLOG/$JOBLOGTMP.`printf %.3d ${TID[${PID[$A]}]}`.log
-			echo >> $DIRLOG/$JOBLOGTMP.`printf %.3d ${TID[${PID[$A]}]}`.log
-			PID[$A]=0
-		fi
+        if $FINALIZO ; then
+            #echo ": FIN salida del Job." >> $DIRLOG/$JOBLOGTMP.`printf %.3d ${TID[${PID[$A]}]}`.log
+            echo "::: Job #${TID[${PID[$A]}]} - Thread #$A - `date +'%d/%m/%Y - %H:%M:%S'` - FINALIZADO." >> $DIRLOG/$JOBLOGTMP.`printf %.3d ${TID[${PID[$A]}]}`.log
+            echo >> $DIRLOG/$JOBLOGTMP.`printf %.3d ${TID[${PID[$A]}]}`.log
+            echo "::: Job #${TID[${PID[$A]}]} - Thread #$A - `date +'%d/%m/%Y - %H:%M:%S'` - FINALIZADO."  >> $LOGCPU
+             cat $DIRLOG/$JOBLOGTMP.`printf %.3d ${TID[${PID[$A]}]}`.log
+            echo >> $DIRLOG/$JOBLOGTMP.`printf %.3d ${TID[${PID[$A]}]}`.log
+            PID[$A]=0
+        fi
 
-		# Salgo del ciclo principal si todas las tareas fueron hechas.
-		if [ ${PID[$A]} -gt 0 ]; then
-			TERMINO=false
-			A=$(($PARALELO-1))
-		fi
+        # Salgo del ciclo principal si todas las tareas fueron hechas.
+        if [ ${PID[$A]} -gt 0 ]; then
+            TERMINO=false
+            A=$(($PARALELO-1))
+        fi
 
-		A=$(($A+1))
+        A=$(($A+1))
 
-	done
+    done
 done
 
 echo ":::: FIN CORRIDA (`date +'%d/%m/%Y - %H:%M:%S'`)" >> $LOGCPU
