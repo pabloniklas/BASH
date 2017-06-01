f913c7d03afeac3c736cecddbc6ae80a13bbfa80
Pablo Niklas <pablo.niklas@gmail.com>
2017-06-01 09:06:56 -0300

    Create centos_local_mirror.sh
    Initial load.

---
+++ centos_local_mirror.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+RSYNC_HOST="rsync://mirrors.rit.edu/centos/"
+RSYNC_PROXY=""
+
+until rsync -avzSH 
+            --delete  
+            --exclude "local*" 
+            --exclude "isos" 
+            --exclude "/2*/" 
+            --exclude "/3*/" 
+            --exclude "/4*/" 
+            --exclude "/5*/" 
+            $RSYNC_HOST /u01/CentOS; do 
+    sleep 1
+done
