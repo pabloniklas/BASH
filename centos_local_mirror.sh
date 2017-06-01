8bd5df4076489ec4d5ff6bec0cb015f439cadfba
Pablo Niklas <pablo.niklas@gmail.com>
2017-06-01 09:12:44 -0300

    Update centos_local_mirror.sh
    Fix.

--- centos_local_mirror.sh
+++ centos_local_mirror.sh
@@ -3,14 +3,6 @@
 RSYNC_HOST="rsync://mirrors.rit.edu/centos/"
 RSYNC_PROXY=""
 
-until rsync -avzSH 
-            --delete  
-            --exclude "local*" 
-            --exclude "isos" 
-            --exclude "/2*/" 
-            --exclude "/3*/" 
-            --exclude "/4*/" 
-            --exclude "/5*/" 
-            $RSYNC_HOST /u01/CentOS; do 
+until rsync -avzSH --delete --exclude "local*" --exclude "isos" --exclude "/2*/" --exclude "/3*/" --exclude "/4*/" --exclude "/5*/" $RSYNC_HOST /u01/CentOS; do 
     sleep 1
 done
